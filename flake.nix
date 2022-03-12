de8f5f035d7ade6dd5e22d89f687afb4cc859bbc
hugosenari <hugosenari@gmail.com>
2022-03-12 02:24:39 -0300

    chore: re index osdialog

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''Nim wrapper for the osdialog library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."osdialog-master".url = "path:./master";
+  inputs."osdialog-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."osdialog-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."osdialog-v0_1_0".url = "path:./v0_1_0";
+  inputs."osdialog-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."osdialog-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim wrapper for the osdialog library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-osdialog-master.flake = false;
+  inputs.src-osdialog-master.owner = "johnnovak";
+  inputs.src-osdialog-master.ref   = "refs/heads/master";
+  inputs.src-osdialog-master.repo  = "nim-osdialog";
+  inputs.src-osdialog-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-osdialog-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-osdialog-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
